9030b9f4485dd9b693c2eca7f5e46662c147b54b
AndRiaBX <babukhadia.andria@gmail.com>
2025-11-08 19:14:52 +0400

    new.py

--- new.py
+++ new.py
@@ -3,13 +3,14 @@ import os
 import shutil
 
 # აქ უნდა ჩაწერო ფოლდერის მისამართი
-mainfolder = r"C:\Users\User\OneDrive\Desktop\pythontr2"
+mainfolder = r""
+# აქ უნდა ჩაწერო იმ ფოლდერის მისამართი სადაც გინდა შეიქმნას ქვეფოლდერები,
 newmainfolder = r"C:\Users\User\OneDrive\Desktop\pythontraining"
 
-newfolder1 = os.path.join(newmainfolder, "new1")
+newfolder1 = os.path.join(newmainfolder, "new1")  # ვქმნით ახალ ფოლდერს
 newfolder2 = os.path.join(newmainfolder, "new2")
 
-os.makedirs(newfolder1, exist_ok=True)
+os.makedirs(newfolder1, exist_ok=True)  # ვამოწმებთ რომ არსებობს
 os.makedirs(newfolder2, exist_ok=True)
 
 
@@ -17,9 +18,10 @@ for root, dirs, files in os.walk(mainfolder):
     for file in files:
         file_path = os.path.join(root, file)
 
-        if file.startswith('dog'):
+        if file.startswith(''):  # აქ ჩაწერე ის რაზეც გინდა ფაილი იწყებოდეს
             shutil.move(file_path, newfolder1)
-        elif file.startswith("cat"):
+        elif file.startswith(""):  # აქ ჩაწერე ის რაზეც გინდა სხვა ტიპის ფაილი იწყებოდეს
             shutil.move(file_path, newfolder2)
 
 print("done")
+
